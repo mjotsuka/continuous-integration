b03d8be951d7f683a52ee9d06e37b136258f50ae
Megan Otsuka <motsuka@students.pitzer.edu>
2021-02-01 14:44:43 -0800

    fixed triangle function code

--- Fixme.py
+++ Fixme.py
@@ -20,7 +20,7 @@ def factorial(n):
     '''
     result = 1
     for i in range(2, n+1):
-        result * =i
+        result *= i
     return result
 
 
@@ -46,5 +46,5 @@ def triangular(n):
     '''
     result = 1
     for i in range(2, n+1):
-        result + =i
+        result += i
     return result
